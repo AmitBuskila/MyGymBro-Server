6c33ac472df597fbd80905041c3bb90ba3afc149
Amit Buskila <amit1bus@gmail.com>
2025-02-27 20:39:04 +0200

    initialize project with TypeScript, Express ; add user authentication and configuration files

---
+++ src/app/app.ts
@@ -0,0 +1,35 @@
+import cors from 'cors';
+import type { Application, Request, Response } from 'express';
+import express from 'express';
+import config from '../config';
+import userRoutes from '../routes/user.routes';
+import { CustomError } from '../utils/customError';
+
+const app: Application = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: [config.clientUrl || ''],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }),
+);
+
+app.get('/', (_req: Request, res: Response) => {
+  res.status(200).json({  message: 'Server is Running! 🏃' });
+});
+
+app.use('/v1/api/users', userRoutes);
+
+app.use((req, res, next) => {
+  const error = new CustomError('Requested URL Not Found', 404);
+  next(error);
+});
+
+
+app.all('*', (_req: Request, res: Response) => {
+  res.status(404).json({ success: false, message: 'Page Not Found' });
+});
+
+export default app;
