879dbf186729d69c959f45b69967954165ef3562
Amit Buskila <amit1bus@gmail.com>
2025-03-11 18:05:23 +0200

    base complex querey

--- src/dal/exercise.dal.ts
+++ src/dal/exercise.dal.ts
@@ -1,4 +1,5 @@
 import { Exercise } from '../entities/exercise.entity';
+import { Workout } from '../entities/workout.entity';
 import { WorkoutExercise } from '../entities/workoutExercise.entity';
 import { AppDataSource } from '../helpers/dataSource';
 import { addWorkoutExerciseInput } from '../types/types';
@@ -13,4 +14,27 @@ export const addWorkoutExercise = async (
   const newTemplate = new WorkoutExercise();
   Object.assign(newTemplate, template);
   return AppDataSource.getRepository(WorkoutExercise).save(newTemplate);
+};
+
+export const getData = (userId: number, exerciseIds: number[]) => {
+  const rankedWorkoutsQuery = AppDataSource.getRepository(Workout)
+    .createQueryBuilder('workout')
+    .select([
+      'workoutExercise.exerciseId AS exerciseId',
+      'workout.id AS workoutId',
+      'workout.startDate AS workoutDate',
+      'ROW_NUMBER() OVER (PARTITION BY workoutExercise.exerciseId ORDER BY workout.startDate DESC) AS rowNumber',
+    ])
+    .innerJoin('workout.workoutExercises', 'workoutExercise')
+    .where('workout.userId = :userId', { userId })
+    .andWhere('workoutExercise.exerciseId IN (:...exerciseIds)', {
+      exerciseIds,
+    });
+
+  return AppDataSource.createQueryBuilder()
+    .select(['ranked.exerciseId', 'ranked.workoutId', 'ranked.workoutDate'])
+    .from(`(${rankedWorkoutsQuery.getQuery()})`, 'ranked')
+    .where('ranked.rowNumber = 1')
+    .setParameters(rankedWorkoutsQuery.getParameters())
+    .getRawMany();
 };
