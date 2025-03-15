87eee4e8bfb951700b4b1b656552c86e42103884
Amit Buskila <amit1bus@gmail.com>
2025-03-15 21:21:19 +0200

    used neon db for remote use

--- src/helpers/dataSource.ts
+++ src/helpers/dataSource.ts
@@ -1,12 +1,8 @@
 import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
+  url: process.env.POSTGRES_URL,
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'Amit2004!',
-  database: 'postgres',
   synchronize: true,
   logging: false,
   schema: 'dev',
