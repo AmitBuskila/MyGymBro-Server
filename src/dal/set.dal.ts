68d618145acbf8f9deb3f2349c558cb63c06b2bb
Amit Buskila <amit1bus@gmail.com>
2025-03-21 00:38:55 +0200

    refactor addWorkoutExercise and addSet functions to log added exercises and sets

--- src/dal/set.dal.ts
+++ src/dal/set.dal.ts
@@ -7,5 +7,7 @@ const setsRepository = AppDataSource.getRepository(Set);
 export const addSet = async (workout: AddSetInput): Promise<Set> => {
   const newSet = new Set();
   Object.assign(newSet, workout);
-  return setsRepository.save(newSet);
+  const addedSet = await setsRepository.save(newSet);
+  console.log('new set has been added', { addedSet });
+  return addedSet;
 };
